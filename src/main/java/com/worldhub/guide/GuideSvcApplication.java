d4bd80b24f3cb27bfcb3cec9b42b1d9f723d4fb2
Peter <guitaronline89@gmail.com>
2025-11-24 13:55:24 +0200

    initial commit

---
+++ src/main/java/com/worldhub/guide/GuideSvcApplication.java
@@ -0,0 +1,13 @@
+package com.worldhub.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuideSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worldhub/guide/GuideSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldhub.guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
