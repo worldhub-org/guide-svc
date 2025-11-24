50b7077cb950b3c5fb5293977a63feb2388bb8b0
Peter <guitaronline89@gmail.com>
2025-11-24 14:01:36 +0200

    initial commit

--- src/main/java/com/worldhub/guide/Application.java
+++ src/main/java/com/worldhub/guide/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GuideSvcApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GuideSvcApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
